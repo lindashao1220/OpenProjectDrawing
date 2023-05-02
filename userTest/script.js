de8b5cb12c5cfddd51768fc581d7bafd8c76f5bf
lindashao1220 <ys4950@nyu.edu>
2023-05-02 17:49:21 +0800

    asdasdd

--- userTest/script.js
+++ userTest/script.js
@@ -335,9 +335,10 @@ if (phase >= period) {
  phase = 0;
 }
 
-if(g >= 50 && g >= 50){
+if(g <= 50 && g >= -50){
 xShift = 50*sin(2*PI*phase/period); //amplitude = 200pxs, 4s a round
 yShift = 2*(1 + cos(4*PI*phase/period));  //imitate some up and down
+
 }else if(g<-50){
  if(phase != 90){
    phase = phase + 2
@@ -361,6 +362,8 @@ xShift = 50*sin(2*PI*phase/period); //amplitude = 200pxs, 4s a round
 yShift = 2*(1 + cos(4*PI*phase/period));  //imitate some up and down
  
 }
+
+
 // translate(width/2 - xShift, yCenter - yShift);
 
 //draw stick
