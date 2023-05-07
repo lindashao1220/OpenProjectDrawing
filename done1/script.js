46103f44ac6abb505a96ad01b4ba039ee27f0cbf
lindashao1220 <ys4950@nyu.edu>
2023-05-07 22:52:48 +0800

    hhh

--- done1/script.js
+++ done1/script.js
@@ -360,13 +360,13 @@ if (phase >= period) {
 }
 
 // if(g <= 50 && g >= 2 && g >= -50 && g <= -2){
-if(g <= 50  &&  g >= -50 && g> 1 && g<-1){
+if(g <= 50  &&  g >= -50 && g >= 1 && g <= -1){
 xShift = 50 * sin(2*PI*phase/period); //amplitude = 200pxs, 4s a round
 yShift = 2*(1 + cos(4*PI*phase/period));  //imitate some up and down
 
 }
 
-if(g<=1 && g>=-1){
+if(g<1 && g>-1){
   if(phase != 180){
     phase = phase + 5
   }if(phase>=180 && phase<=190){
