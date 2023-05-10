cf051c4a7bcf85f728175db513cd9bfd73bc62eb
lindashao1220 <ys4950@nyu.edu>
2023-05-10 21:52:10 +0800

    dwd

--- done1/script.js
+++ done1/script.js
@@ -548,7 +548,7 @@ if (showText) {
   fill(0)
   textSize(20);
   textAlign(CENTER, CENTER);
-  text("⬇️try blowing the microphone⬇️", width/2, height-25);
+  text("⬇️try blowing the dandelion using microphone⬇️", width/2, height-25);
   text("✨try sway a little✨", width/3.5, 50);
 }
 
